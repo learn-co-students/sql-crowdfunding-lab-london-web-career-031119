6aa940b08fa759fe7cfc25dd25f65826cbc96fed
Umar Khan <umarkhandev@gmail.com>
2019-03-22 09:17:50 +0000

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -30,7 +30,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
     JOIN pledges
     ON pledges.project_id = projects.id
     GROUP BY projects.title
-    ORDER BY SUM(pledges.amount) - projects.funding_goal > -1 DESC LIMIT 2";
+    HAVING SUM(pledges.amount) >= projects.funding_goal";
 
 end
 
